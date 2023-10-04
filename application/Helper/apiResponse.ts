391398cd297e4ca107db7665800f3103b311cfdd
Md Mustakim <mdmustakim.dev@gmail.com>
2023-10-04 23:24:46 +0600

    responseExample

--- application/Helper/apiResponse.ts
+++ application/Helper/apiResponse.ts
@@ -1,11 +1,27 @@
+import {Response} from "express";
+
 const apiResponse = {
-    success: (res: any, data: any, message: string) => {
-        const result = {
-            status: 200,
+    structure: ( res: Response, statusCode: number, status: string, data: any, message: string) => {
+        return res.status(statusCode).json({
+            status,
             message,
             data
-        };
-        return res.status(200).json(result);
+        });
+    },
+    success: (res: Response, data: any, message: string) => {
+        return apiResponse.structure(res, 200, "Success", data, message);
+    },
+    created: (res: Response, data: any, message: string) => {
+        return apiResponse.structure(res, 201, "Created", data, message);
+    },
+    notFound: (res: Response, data: any, message: string) => {
+       return apiResponse.structure(res, 404, "Not Found", data, message);
+    },
+    badRequest: (res: Response, data: any, message: string) => {
+        return apiResponse.structure(res, 400, "Bad Request", data, message);
+    },
+    unauthorized: (res: Response, data: any, message: string) => {
+        return apiResponse.structure(res, 401, "Unauthorized", data, message);
     }
 }
 
--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import express, { Express, Request, Response } from 'express';
+import apiResponse from "./application/Helper/apiResponse";
 
 
 const app: Express = express();
 const port = 4000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    apiResponse.success(res, { name: 'John Doe' }, 'Success');
 });
 
 app.listen(port, () => {
