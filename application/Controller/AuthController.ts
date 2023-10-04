bf1bb68f7e5462707d2948b729b9dc1419916d34
Md Mustakim <mdmustakim.dev@gmail.com>
2023-10-04 23:57:50 +0600

    ok

--- application/Controller/AuthController.ts
+++ application/Controller/AuthController.ts
@@ -3,18 +3,8 @@ import Connection from "../Database/Connection";
 const AuthController = {
     login: (req: Request, res: Response) => {
 
-        Connection.getConnection().then((connection) => {
-            connection.query('SELECT * FROM users', (err: any, rows: any) => {
-                if (err) {
-                    console.log(err);
-                }
-                console.log(rows);
-                res.setHeader('Content-Type', 'application/json');
-                res.json(rows);
 
-            });
-        });
-        // res.send('Login');
+         res.send(req.body);
     }
 }
 
---
+++ application/Controller/UserController.ts
@@ -0,0 +1,21 @@
+import  { Request, Response } from 'express';
+import Connection from "../Database/Connection";
+const UserController = {
+    index: (req: Request, res: Response) => {
+        Connection.getConnection().then((connection) => {
+            connection.query('SELECT * FROM users', (err: any, rows: any) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log(rows);
+                res.setHeader('Content-Type', 'application/json');
+                res.json(rows);
+
+            });
+        });
+
+        // res.send('Login');
+    }
+}
+
+export default UserController;
