1d691a9d627577f49e22a00f4e011f3f47c55c3a
Md Mustakim <mdmustakim.dev@gmail.com>
2023-10-11 23:56:37 +0600

    ok

--- application/Controller/AuthController.ts
+++ application/Controller/AuthController.ts
@@ -2,27 +2,15 @@ import  { Request, Response } from 'express';
 
 import apiResponse from "../Helper/apiResponse";
 
-
-
-
 const AuthController = {
     login: (req: Request, res: Response) => {
         console.log('ok');
         return apiResponse.success(res, 'Login is Successful', []);
     },
     register: (req: Request, res: Response) => {
-
-
-
     },
     verifyPass(req: Request, res: Response) {
-        // const query = "SELECT * FROM users where id = 2";
-        //
-        // const hashPassword = "$2y$10$q0cymLmu.b/nmyfULrahUe2fYG2jAWBvVnrwLJnmfcZ6/1ghvNQDK";
-        // const plaintPassword = "Admin#1689s9";
-        // const passwordVerify = Model.passwordVerify(plaintPassword, hashPassword);
-        //
-        // res.json({passwordVerify: passwordVerify});
+
     }
 }
 
--- application/Router/api/v1/authenticatedRouter.ts
+++ application/Router/api/v1/authenticatedRouter.ts
@@ -1,5 +1,10 @@
-import express, {Router} from "express";
+import express, {  Request, Response, Router } from 'express';
 import {VerifyToken} from "../../../Middleware/VerifyToken";
+import {body, validationResult} from "express-validator";
+import multer from "multer";
+import apiResponse from "../../../Helper/apiResponse";
+import HelperFunction from "../../../Helper/HelperFunction";
+
 
 const authenticateRouter: Router = Router();
 
@@ -8,8 +13,38 @@ authenticateRouter.use(express.json());
 
 authenticateRouter.use(VerifyToken);
 
-authenticateRouter.get('/test', (req, res) => {
-    res.send('hello');
+authenticateRouter.get('/organizer', (req, res) => {
+    res.send('get all organizer');
+});
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/organizer');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+authenticateRouter.use(express.static('public'));
+
+authenticateRouter.post('/organizer', upload.single('cover') ,[
+    body('name').notEmpty().withMessage('Name is required'),
+    body('slug').notEmpty().withMessage('Slug is required'),
+    body('description').notEmpty().withMessage('Description is required'),
+
+], (req: Request, res: Response) => {
+
+    const errors = validationResult(req).formatWith(HelperFunction.validationErrorFormat);
+    if (!errors.isEmpty()) {
+        return apiResponse.validationErrorWithData(res, 'Validation Error', errors.array());
+    }
+
+
+
+    res.send('create organizer');
 });
 
 
