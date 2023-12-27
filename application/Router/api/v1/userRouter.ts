f51e31ccdd1dd35483b3e85b9cf3cd0e7428d42e
Md Mustakim <mdmustakim.dev@gmail.com>
2023-12-27 10:31:24 +0600

    master

--- application/Router/api/v1/userRouter.ts
+++ application/Router/api/v1/userRouter.ts
@@ -8,37 +8,24 @@ import Model from "../../../Helper/Model";
 import { VerifyToken } from '../../../Middleware/VerifyToken';
 
 
-const organizerRouter: Router = Router();
+const userRouter: Router = Router();
 
-organizerRouter.use(VerifyToken);
-organizerRouter.use(express.urlencoded({ extended: true }));
-organizerRouter.use(express.json());
+userRouter.use(VerifyToken);
+userRouter.use(express.urlencoded({ extended: true }));
+userRouter.use(express.json());
 
-organizerRouter.get('/', (_req, res) => {
-    
-    const allUserQuery = 'select * from organizers';
-    Model.get(allUserQuery).then((result: any) => {
-
-        result = result.map((organizer: any) => {
-            organizer.cover = HelperFunction.env('APP_URL') + '/public/organizer/' + organizer.cover;
-            return organizer;
-        });
-
-        apiResponse.success(res, 'All Organizer', result);
+userRouter.get('/', (_req, res) => {
+    Model.get('select * from users').then((result: any) => {
+        apiResponse.success(res, 'All Users', result);
     }).catch((err: any) => {
         apiResponse.error(res, 'Error', err);
     });
 });
 
-organizerRouter.get('/:id', (req, res) => {
+userRouter.get('/:id', (req, res) => {
 
-    const params = [req.params.id];
-    const allUserQuery = 'select * from organizers where id = ?';
-    Model.first(allUserQuery, params).then((result: any) => {
-
-        result.cover = HelperFunction.env('APP_URL') + '/public/organizer/' + result.cover;
-
-        apiResponse.success(res, 'Organizer', result);
+    Model.first('select * from users where id = ?', [req.params.id]).then((result: any) => {
+        apiResponse.success(res, 'User', result);
     }).catch((err: any) => {
         apiResponse.error(res, 'Error', err);
     });
@@ -57,64 +44,11 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-organizerRouter.use(express.static('public'));
-organizerRouter.use(express.static('event'));
-
-organizerRouter.post('/',
-    upload.single('cover'), [
-    body('name').notEmpty().withMessage('Name is required'),
-    body('slug').custom(async (value: string) => {
-        let query = "SELECT * FROM organizers WHERE slug = ?";
-        let params = [value];
-        return Model.first(query, params).then((result: any) => {
-            if (result.length > 0) {
-                throw new Error('Slug already in use');
-            }
-        }).catch((err: any) => {
-            throw new Error(err.message);
-        })}),
-    body('description').notEmpty().withMessage('Description is required'),
-], (req: Request, res: Response) => {
-    if(!req.file){
-        return apiResponse.validationErrorWithData(res, 'Validation Error', [{message: 'Cover is required'}]);
-    }
-
-    const errors = validationResult(req).formatWith(HelperFunction.validationErrorFormat);
-    if (!errors.isEmpty()) {
-        return apiResponse.validationErrorWithData(res, 'Validation Error', errors.array());
-    }
-
-    let body = req.body;
-    let {name, slug, description, about} = body;
-    let cover = req.file.filename;
-    
-
-
-    let query = "INSERT INTO organizers (name, slug, description, cover, about, created_u_a, created_ip, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    let params = [name, slug, description, cover, about, req.headers['user-agent'], HelperFunction.getIpAddress(req), HelperFunction.getDateTime(0), HelperFunction.getDateTime(0)];
-    Model.queryExecute(query, params).then((result: any) => {
-        console.log(result);
-
-            
-            return apiResponse.success(res, 'Registration is Successful', []);
-    }).catch((err: any) => {
-        return apiResponse.error(res, err.message, []);
-
-    });
-    }
-
-);
-
-
-
-
-organizerRouter.post('/update/:id', multer().none(),
+userRouter.post('/update/:id', multer().none(),
   (_req: Request, res: Response) => {
-
-    let checkIdQuery = "SELECT * FROM organizers WHERE id = ?";
     let checkIdParams = [_req.params.id];
 
-     Model.get(checkIdQuery, checkIdParams).then((result: any) => {
+     Model.get('SELECT * FROM users WHERE id = ?', checkIdParams).then((result: any) => {
         if (result.length == 0) {
             return apiResponse.notFound(res, 'Organizer not found', []);
         } else {
@@ -150,7 +84,7 @@ organizerRouter.post('/update/:id', multer().none(),
     }
 );
 
-organizerRouter.post('/status-update/:id', multer().none(),
+userRouter.post('/status-update/:id', multer().none(),
     (_req: Request, res: Response) => {
 
         let checkIdQuery = "SELECT * FROM organizers WHERE id = ?";
@@ -192,4 +126,4 @@ organizerRouter.post('/status-update/:id', multer().none(),
         }
     );
 
-export default organizerRouter;
+export default userRouter;
